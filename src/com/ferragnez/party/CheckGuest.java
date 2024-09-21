674e0badf800686aac0d1a1e8f0a05cfe4345c4c
AlinValentin7 <trandafiralinvalentin@gmail.com>
2024-09-21 14:32:58 +0200

    add:codice-verifica

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -1,5 +1,37 @@
 package com.ferragnez.party;
 
-public class CheckGuest {
+import java.util.Scanner;
+/*
 
-}
+Nel programma bisogna:
+creare e inizializzare l’array contenente i nomi degli invitati (Fatto)
+chiedere all’utente come si chiama (fatto)
+verificare che il nome sia presente nella lista lasciarlo entrare
+ o rispedirlo cortesemente da dove è venuto
+*/
+public class CheckGuest {
+	public static void main(String[] args) {
+		String [] guest = { "Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", 
+				 "Francesco Totti", "Ilary Blasi", "Bebe Vio", 
+				 "Luis","Pardis Zarei", "Martina Maccherone", "Rachel Zeilic" };
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Come ti chiami?");
+		String name = scan.nextLine();
+		boolean presente = false;
+		for(int i = 0; i < guest.length; i++) {
+			if(guest[i].equalsIgnoreCase(name)) {
+				presente = true;
+				break;
+			}
+		}
+		if(presente) {
+			System.out.println("Sei il/la Beveneuto/a " + name + " prenditi qulacosa da bere");
+		}else {
+			System.out.println ("Non sei il/la Beveneuto/a " + name + " torna tra i poveri");
+		}			
+		
+			
+			
+											}
+		}
+	
